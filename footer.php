c4695c45e7252e68d81a2d507a84b27b67993993
Hardik-web-wp <sambhavsandeshweb@gmail.com>
2026-07-03 14:00:49 +0530

    Add initial footer.php

---
+++ footer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Theme Footer
+ * @package Samabhav_Sandesh
+ */
+?>
+</main>
+<footer class="site-footer">
+    <div class="container">
+        <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. All rights reserved.</p>
+    </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
